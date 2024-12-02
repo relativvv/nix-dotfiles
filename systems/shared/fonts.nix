29e11923c46a732f23cef00129a3292d1d4dbc31
Kevin Rudde <k.rudde@shopware.com>
2024-12-02 08:48:37 +0100

    Update flake inputs and used new nerd-font package

--- systems/shared/fonts.nix
+++ systems/shared/fonts.nix
@@ -4,11 +4,7 @@
 }: {
   fonts = {
     packages = [
-      ( pkgs.nerdfonts.override {
-        fonts = [
-          "JetBrainsMono"
-        ];
-      } )
+      pkgs.nerd-fonts.jetbrains-mono
     ];
   };
 }
